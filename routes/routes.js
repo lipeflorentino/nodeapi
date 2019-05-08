4a1e07f34c0a1883bbd4335e8f768db4bf7dbb76
Filipe Lima <lipeflorentino@yahoo.com>
2019-05-08 15:59:29 +0000

    atualizando

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ const router = app => {
     // middleware para validar o Token
     app.use((req, res, next) => {
       // Aqui vamos verificar o header da requisição, os parametros e o corpo da requisição, procurando o token
-      var token = req.body.token || req.query.token || req.headers['x-access-token']
+      var token = req.body.token || req.query.token || req.headers['X-Api-Key']
     
       // Se o token existir
       if (token) {
