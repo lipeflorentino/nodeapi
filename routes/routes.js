a06d48ad970e688d5bb9de0947fa2280adfe72b4
Filipe Lima <lipeflorentino@yahoo.com>
2019-05-08 16:45:28 +0000

    atualizando

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ const router = app => {
     // middleware para validar o Token
     app.use((req, res, next) => {
       // Aqui vamos verificar o header da requisição, os parametros e o corpo da requisição, procurando o token
-      var token = req.body.token || req.query.token || req.headers['X-Amz-Security-Token']
+      var token = req.body.token || req.query.token || req.headers['x-amz-security-token']
     
       // Se o token existir
       if (token) {
